6d2dfb386f2841757a86dacc75e9da67922140d3
Yaro <yshmarov@gmail.com>
2022-05-05 20:11:36 +0200

    scaffold posts, generate static_pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def pricing
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts
+  get 'static_pages/home'
+  get 'static_pages/pricing'
+  get 'static_pages/terms'
+  get 'static_pages/privacy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'posts#index'
 end
